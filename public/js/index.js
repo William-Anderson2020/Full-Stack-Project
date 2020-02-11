a5971a2087b43bf416940d8923e4e4295cb9fcb2
kaitonggao <50878288+kaitonggao@users.noreply.github.com>
2020-02-11 11:38:59 -0500

    hmm

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+const app = express();
+
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
+
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Our First Express App"
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+//127.0.0.1:3000
