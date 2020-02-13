54393b8c55615a4235fff9d3fa0d1456e0c83acc
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2020-02-12 19:43:05 -0500

    Tile Highlights Finished
    - Board now highlights tiles within a specified distance of the selected tile

--- src/bundles/board.js
+++ src/bundles/board.js
@@ -1,35 +1,35 @@
 import { DOMELEMENTS } from "./domElements";
 import { clickListener } from "./clickListener";
 
-console.log('link')
-
-let tileArray = [];
+let tileArray = []; //Grabs all tiles on the board, passes into an array with proporties we'll use later.
 DOMELEMENTS.tiles.forEach(el => {
     let tile = {
         dom: el,
-        x: el.getAttribute("x"),
-        y: el.getAttribute("y"),
+        x: parseInt(el.getAttribute("x")),
+        y: parseInt(el.getAttribute("y")),
         occupied: null,
         terrain: null
     };
-    console.log(tile.x, tile.y);
+    //console.log(tile.x, tile.y);
     tileArray.push(tile);
 });
 
-tileArray.forEach(el => {
+tileArray.forEach(el => { //Shows tiles within range.
     el.dom.addEventListener("mouseover", el => {
+        el = el.target;
         tileArray.forEach(tile => {
-            if (Math.abs(tile.x - el.x) + Math.abs(tile.y - el.y) <= 2){
+            if (Math.abs(tile.x - el.getAttribute('x')) + Math.abs(tile.y - el.getAttribute('y')) <= 2){
                 tile.dom.classList.add("viable");
             }
         });
     })
 });
 
-tileArray.forEach(el => {
+tileArray.forEach(el => { //Removes highlight when mouse moved off of tile.
     el.dom.addEventListener("mouseout", el => {
+        el = el.target;
         tileArray.forEach(tile => {
-            if (Math.abs(tile.x - el.x) + Math.abs(tile.y - el.y) <= 2){
+            if (Math.abs(tile.x - el.getAttribute('x')) + Math.abs(tile.y - el.getAttribute('y')) <= 2){
                 tile.dom.classList.remove("viable");
             }
         });
