659439ea3d40a78897a935c48cb86b56f0f72b0d
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2020-02-14 11:44:15 -0500

    Item Done

--- public/index.js
+++ public/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("./db/mongoose");
 const app = express();
 const characterRouter = require("./routers/character")
+const itemRouter = require("./routers/item")
 app.use(express.json());
 app.use(characterRouter)
+app.use(itemRouter)
 
 app.listen(3000, ()=>{
     console.log("3000");
