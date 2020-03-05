036aa4410aa31d1f700e2d01f9b9f69fee9d4fb7
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2020-03-05 11:31:53 -0500

    Weapon and Unit Classes

--- src/bundles/unit.js
+++ src/bundles/unit.js
@@ -1,10 +1,18 @@
 class Unit{
-    constructor(stats, start){
+    constructor(stats, start, owner, weapon=null){
         this.stats = stats;
         this.pos = {
             x: start.x,
             y: start.y
         };
+        this.owner = owner
         this.active= true;
+        this.weapon=null
+    }
+    active(){
+        this.active=!(this.active)
+    }
+    weapon(weapon){
+        this.weapon=weapon
     }
 }
