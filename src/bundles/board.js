a129a252a365fc8cc908fc20a78c70655c05f550
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2020-03-25 16:28:08 -0400

    Damage Calculation
    -Added damage calculation
    --HP diference doesn't appear on UI, but logs confirm the objects are being changed.
    
    -Attack animation flip issue has been fixed
    
    -Next up: Followup attacks or Turn base and socket.io intergration

--- src/bundles/board.js
+++ src/bundles/board.js
@@ -21,7 +21,11 @@ const amelia = {
     "img": "./img/ameliaIdle.png",
     "anim": "./img/ameliaAtk.gif",
     "name": "Amelia",
+    "hp": 20,
+    "weapon": "spear",
     "stats":{
+        "atk": 3,
+        "def": 2,
         "mvt": 2,
         "rng": 1
     },
@@ -30,6 +34,13 @@ const amelia = {
         "y": 5,
         "tile": {}
     },
+    "item":{
+        "name": "Training Sword",
+        "stats": {
+            "atk": 1,
+            "hp": 3
+        }
+    },
     "owner": "Player 1"
 };
 
@@ -37,7 +48,11 @@ const erika = {
     "img": "./img/erikaIdle.png",
     "anim": "./img/erikaAtk.gif",
     "name": "Erika",
+    "hp": 25,
+    "weapon": "sword",
     "stats":{
+        "atk": 2,
+        "def": 1,
         "mvt": 3,
         "rng": 1
     },
@@ -45,6 +60,13 @@ const erika = {
         "x": 6,
         "y": 5,
         "tile": {}
+    },
+    item:{
+        name: "Wooden Shield",
+        stats: {
+            "atk": 1,
+            "def": 2
+        }
     },
     "owner": "Player 2"
 };
@@ -60,7 +82,9 @@ tiles.forEach(el => {
             "isOccupied": false,
             "unit": {}
         },
-        terrain: {}
+        terrain: {
+            stats: {}
+        }
     };
     //console.log(tile.x, tile.y);
     tileArray.push(tile);
@@ -118,17 +142,17 @@ function tilesInRange(tile, dist){
 function gameInit(){
     tileArray.forEach(el =>{
         el.dom.addEventListener("click", turnInit);
-    })
-}
+    });
+    unitTileArray.forEach(tile => { //Apply one-time hp buffs
+        let unit = tile.occupied.unit;
+        if(unit.item.stats.hp){
+            unit.hp += unit.item.stats.hp;
+        };
+    });
+};
 
-let unit;
-
-function turnInit(el){
-    let tile = getTile(el.target);
-
-    function battleRes(attacker, defender){
-        //console.log(attacker, defender);
-        let movementTiles = tilesInRange(defender.pos.tile, attacker.stats.rng);
+function battleDisp(attacker, defender){
+    let movementTiles = tilesInRange(defender.pos.tile, attacker.stats.rng);
         let atkTile = movementTiles[0];
         if(Math.abs(attacker.pos.x - defender.pos.x) + Math.abs(attacker.pos.y - defender.pos.y) > 1){
             movementTiles.forEach(m => {
@@ -136,7 +160,6 @@ function turnInit(el){
                     atkTile = m;
                 };
             });
-            console.log(atkTile);
             attacker.pos.x = atkTile.x;
             attacker.pos.y = atkTile.y;
             if(defender.pos.x > attacker.pos.x && !attacker.pos.tile.dom.classList.contains("flip")){
@@ -146,11 +169,141 @@ function turnInit(el){
         };
         attacker.pos.tile.dom.innerHTML = `<img class="board_sprite anim_sprite" src="${attacker.anim}">`;
         if(attacker.pos.x < defender.pos.x){
-            attacker.pos.tile.dom.innerHTML.classList.add("flip");
+            if(!attacker.pos.tile.dom.classList.contains("flip")){
+                attacker.pos.tile.dom.classList.add("flip");
+            }
+        }else{
+            if(attacker.pos.tile.dom.classList.contains("flip")){
+                attacker.pos.tile.dom.classList.remove("flip");
+            }
         };
-        setTimeout(r => {dispUnit(attacker)}, 1600);
+        setTimeout(r => {dispUnit(attacker)}, 1600); /*Fix timing*/
+}
 
-    };
+function buffCalc(unit){
+    let uB = Object.assign({}, unit.stats)
+    let buffCounters = [];
+    if(unit.item.stats != {}){buffCounters.push(unit.item.stats)};
+    if(unit.pos.tile.terrain.stats != {}){buffCounters.push(unit.pos.tile.terrain.stats)};
+
+    buffCounters.forEach(buff => {
+        Object.keys(buff).forEach(key => {
+            uB[key] += buff[key];
+        });
+    });
+    return uB;
+}
+
+function getWeapon(unit){
+   let weapon;
+   switch(unit.weapon){
+       case "sword":
+           weapon = "t1";
+           break;
+        case "magic":
+            weapon = "t1";
+            break;
+        case "spear":
+            weapon = "t2";
+            break;
+        case "axe":
+            weapon = "t3";
+            break;
+        case "bow":
+            wepaon = "t3";
+            break;
+        default:
+            weapon = "t2";
+            break;
+   };
+   return weapon;
+};
+
+function damageCalc(attacker, defender){
+    args = [...arguments];
+    args.forEach(arg => {arg.cStats = buffCalc(arg)}); /*Use cStats for battle*/
+  
+    let adv = 1;
+    let crit = 1;
+    let mit = 0;
+
+    if(attacker.weapon == "magic"){ /*mitigation factor*/
+        mit = defender.cStats.res;
+    } else{
+        mit = defender.cStats.def;
+    }
+
+    /*weapon triangle*/
+    let weaponA = getWeapon(attacker);
+    let weaponD = getWeapon(defender);
+    switch(weaponA){
+        case "t1":
+            switch(weaponD){
+                case "t1":
+                    break
+                case "t2":
+                    adv -= .2;
+                    break;
+                case "t3":
+                    adv += .2;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "t2":
+            switch(weaponD){
+                case "t1":
+                    adv += .2;
+                    break;
+                case "t2":
+                    break;
+                case "t3":
+                    adv -= .2;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "t3":
+            switch(weaponD){
+                case "t1":
+                    adv -= .2;
+                    break;
+                case "t2":
+                    adv += .2;
+                    break;
+                case "t3":
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            console.log("weapon trianlge defaulting.");
+    }
+
+
+    if(Math.trunc(Math.random()*100) < (attacker.cStats.luck + attacker.cStats.luck - defender.cStats.luck)/2 ){ /*crit calc*/
+        crit = 1.5;
+    }
+
+    damage = (attacker.cStats.atk + Math.trunc(attacker.cStats.atk * adv) - mit) * crit;
+    console.log(damage);
+    defender.hp -= damage;
+
+    console.log(attacker.hp, defender.hp);
+};
+
+function battleRes(attacker, defender){
+    battleDisp(attacker, defender);
+    damageCalc(attacker, defender);
+};
+
+let unit;
+
+function turnInit(el){
+    let tile = getTile(el.target);
     
     if(tile.occupied.isOccupied == true){
         if(tile.dom.classList.contains("atkViable") && tile.occupied.unit.owner != unit.owner){
