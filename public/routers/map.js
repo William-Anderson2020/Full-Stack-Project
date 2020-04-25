c0394a35ba111a6c13dac6b8f6ca5b2ccbb1e422
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2020-04-25 03:27:23 -0400

    Update map.js

--- public/routers/map.js
+++ public/routers/map.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const Map = require("../models/map");
+const upload = require("../middleware/upload");
+const router = new express.Router();
 
+router.post("/maps", async(req,res)=>{
+    try {
+        const map = new Map(req.body)
+        await map.save();
+        res.send(map)
+    } catch (error) {
+     res.status(501).send(error)   
+    }
+})
+
+router.get("/maps", async(req,res)=>{
+    try{
+        let map = await Map.find(req.body);
+        res.send(map)
+    }catch(error){
+        res.status(501).send(error)
+    }
+})
+
+
+module.exports= router;
