90f46f55a5b83d091787cf6f3eff102bdc7e8bd3
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2020-04-26 13:37:39 -0400

    Add mongoose to .env
    - remove old admin at post

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
+
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config();
+  }
+
+
 mongoose.connect(
-    "mongodb+srv://admin:lwkws38483@full-stack-db-wbsq7.mongodb.net/test?retryWrites=true&w=majority",{
+    `mongodb+srv://${env.DBID}:${env.DBPASS}@full-stack-db-wbsq7.mongodb.net/test?retryWrites=true&w=majority`,{
     useNewUrlParser:true,
     useCreateIndex: true,
     useFindAndModify: true,
