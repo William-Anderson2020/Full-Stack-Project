fcd377d097450771ab3a2fee9473b04ac34fe09d
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2020-04-26 14:34:51 -0400

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV !== 'production'){
 
 
 mongoose.connect(
-    `mongodb+srv://${env.DBID}:${env.DBPASS}@full-stack-db-wbsq7.mongodb.net/test?retryWrites=true&w=majority`,{
+    `mongodb+srv://${process.env.DBID}:${process.env.DBPASS}@full-stack-db-wbsq7.mongodb.net/test?retryWrites=true&w=majority`,{
     useNewUrlParser:true,
     useCreateIndex: true,
     useFindAndModify: true,
